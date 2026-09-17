ffd17013a9618b160da93d04a317e9aa687eb60b
Malin Hallgren <malin.m.hallgren@gmail.com>
2026-09-17 15:46:20 +0200

    deploy hotfix

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,12 +6,12 @@ import fs from 'fs'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      cert: fs.readFileSync(path.resolve(import.meta.dirname, 'certs', 'localhost.crt')),
-      key: fs.readFileSync(path.resolve(import.meta.dirname, 'certs', 'localhost.key'))
-    },
-    port: 5173,
-    host: 'localhost'
-  }
+  // server: {
+  //   https: {
+  //     cert: fs.readFileSync(path.resolve(import.meta.dirname, 'certs', 'localhost.crt')),
+  //     key: fs.readFileSync(path.resolve(import.meta.dirname, 'certs', 'localhost.key'))
+  //   },
+  //   port: 5173,
+  //   host: 'localhost'
+  // }
 })
